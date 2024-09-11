526d710fd7f37b7b814535b5905045fe4599adf1
suvarna1228 <suvu1228@gmail.com>
2024-09-11 12:09:36 +0530

    learned undefined and not defined in js

--- 1.js
+++
@@ -1,7 +0,0 @@
-var x =7
-function getname(){
-    console.log("Namaste javascript");
-}
-getname();
-console.log(x);
-console.log(getname);
---
+++ index.js
@@ -0,0 +1,9 @@
+var x;
+console.log(x);
+var x=10;
+if (x===undefined){
+    console.log("x is undefined");
+}
+else{
+    console.log("x is defined to some value")
+}
