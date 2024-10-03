f6551addc3a857bd99c66c9de9f3c05a5fa00163
suvarna1228 <suvu1228@gmail.com>
2024-10-03 13:50:29 +0530

    promise in js

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
-console.log("start");
+// const p1=new Promise((resolve,reject) => {
+//     setTimeout(() => resolve("p1 sucess"),3000);
+        
+// })
+// const p2=new Promise((resolve,reject) => {
+//     setTimeout(() => resolve("p2 sucess"),1000);
+//     // setTimeout(() => reject("p2 Fail"),1000);
+        
+// })
+// const p3=new Promise((resolve,reject) => {
+//         setTimeout(() => resolve("p3 sucess"),2000);
+            
+// })
 
-setTimeout(function Cb() {
-    console.log("callback");
-}, 5000);
+// Promise.any([p1,p2,p3]).then(res =>{
+//     console.log(res);
+// })
+// .catch((err) => {
+//     console.error(err);
+// });
+const p = new Promise((resolve,reject)=>{
+    resolve("hi");
+});
 
-console.log("end");
-
-
-let startDate = new Date().getTime(); 
-let endDate = startDate;
-while (endDate < startDate +10000) {
-   endDate = new Date().getTime(); 
+async function handlePromise() {
+   const val = await p;
+   console.log(val);
 }
+ handlePromise();
+// const data =getData();
 
-
+// data.then((res)=>console.log(res));
